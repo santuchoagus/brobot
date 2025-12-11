871d3f3b72559a3ec3dc5a2cc8fab413a0519d1d
santuchoagus <97124374+santuchoagus@users.noreply.github.com>
2025-12-11 09:58:55 -0300

    Fix temporal comandos dentro de comunidades

--- routes.go
+++ routes.go
@@ -9,15 +9,15 @@ var prefix = "bro"
 
 func RegisterRoutes(b *bot.Bot) {
 	// TODO: Esto es un asco pero funciona por ahora, habria que guardarlo en una DB o un json.
-	b.RegisterHandler(bot.HandlerTypeMessageText, "plan", bot.MatchTypeCommand, handlers.HandlePlan)
+	b.RegisterHandler(bot.HandlerTypeMessageText, "/plan", bot.MatchTypePrefix, handlers.HandlePlan)
 	b.RegisterHandler(bot.HandlerTypeMessageText, prefix+" plan", bot.MatchTypePrefix, handlers.HandlePlan)
 	b.RegisterHandler(bot.HandlerTypeMessageText, prefix+" flan", bot.MatchTypePrefix, handlers.HandleFlan)
 
-	b.RegisterHandler(bot.HandlerTypeMessageText, "links", bot.MatchTypeCommand, handlers.HandleLinks)
+	b.RegisterHandler(bot.HandlerTypeMessageText, "/links", bot.MatchTypePrefix, handlers.HandleLinks)
 	b.RegisterHandler(bot.HandlerTypeMessageText, prefix+" links", bot.MatchTypePrefix, handlers.HandleLinks)
 
-	b.RegisterHandler(bot.HandlerTypeMessageText, "contribuir", bot.MatchTypeCommand, handlers.HandleContribuir)
+	b.RegisterHandler(bot.HandlerTypeMessageText, "/contribuir", bot.MatchTypePrefix, handlers.HandleContribuir)
 	b.RegisterHandler(bot.HandlerTypeMessageText, prefix+" contribuir", bot.MatchTypePrefix, handlers.HandleContribuir)
 
-	b.RegisterHandler(bot.HandlerTypeMessageText, "materias", bot.MatchTypeCommand, handlers.HandleMaterias)
+	b.RegisterHandler(bot.HandlerTypeMessageText, "/materias", bot.MatchTypePrefix, handlers.HandleMaterias)
 }
